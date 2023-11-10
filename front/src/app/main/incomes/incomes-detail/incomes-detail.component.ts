9263cc530ecfc386cf1f505c81278b0722b103a8
Ross-X <Rossi Andreina Perez Araque>
2023-11-10 09:52:32 +0100

    Se corrige el modal de home el caret-color

--- front/src/app/main/incomes/incomes-detail/incomes-detail.component.ts
+++ front/src/app/main/incomes/incomes-detail/incomes-detail.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ValidatorFn } from '@angular/forms';
-import { OValidators } from 'ontimize-web-ngx';
+import { Component, OnInit, Inject } from "@angular/core";
+import { ValidatorFn } from "@angular/forms";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { OValidators } from "ontimize-web-ngx";
 
 @Component({
-  selector: 'app-incomes-detail',
-  templateUrl: './incomes-detail.component.html',
-  styleUrls: ['./incomes-detail.component.css']
+  selector: "app-incomes-detail",
+  templateUrl: "./incomes-detail.component.html",
+  styleUrls: ["./incomes-detail.component.css"],
 })
 export class IncomesDetailComponent implements OnInit {
   validatorAmount: ValidatorFn[] = [];
-  constructor() { 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<IncomesDetailComponent>
+  ) {
     this.validatorAmount.push(
-      OValidators.patternValidator(/^\d+([,.]\d+)?$/, 'negativeNumber'),
+      OValidators.patternValidator(/^\d+([,.]\d+)?$/, "negativeNumber")
     );
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+  onUpdate(event: any) {
+    this.dialogRef.close();
   }
-
 }
