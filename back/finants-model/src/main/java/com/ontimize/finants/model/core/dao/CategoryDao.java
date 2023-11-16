2a57c44e5497a0c7fefac91433da94e39498e22d
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-11-16 10:55:24 +0100

    Corrección bug CategoryDao

--- back/finants-model/src/main/java/com/ontimize/finants/model/core/dao/CategoryDao.java
+++ back/finants-model/src/main/java/com/ontimize/finants/model/core/dao/CategoryDao.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 @Lazy
 @ConfigurationFile(configurationFile = "dao/CategoryDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class CategoryDao extends OntimizeJdbcDaoSupport {
-    public static final String ATTR_CA_ID = "CA_EX_ID";
-    public static final String ATTR_CA_NAME = "CA_EX_NAME";
+    public static final String ATTR_CA_ID = "CA_ID";
+    public static final String ATTR_CA_NAME = "CA_NAME";
     public static final String ATTR_CA_ISEXPENSES = "CA_ISEXPENSES";
     public static final String ATTR_CA_ISINCOMES = "CA_ISINCOMES";
     public static final String QUERY_CATEGORIES_EXPENSES= "categoriesExpenses";
